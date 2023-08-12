1493ac5abe5b590c565e0c8f51b721f1dae1c55e
ove2xrate <133969780+ove2xrate@users.noreply.github.com>
2023-08-12 18:40:14 +0400

    финал

--- image_feed/ImagesList/ImagesListCell.swift
+++ image_feed/ImagesList/ImagesListCell.swift
@@ -1,18 +1,11 @@
-//
-//  ImagesListCell.swift
-//  image_feed
-//
-//  Created by Ivan Ryabov on 10/08/2023.
-//
-
 import Foundation
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-    // MARK: - Private Properties
+// MARK: - Private Properties
     static let reuseIdentifier = "ImagesListCell"
     
-    // MARK: - IBOutlet
+// MARK: - IBOutlet
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var dateLabel: UILabel!
