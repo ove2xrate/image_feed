19c12bb9683b84cbf2169960b05ce8ecae3e9cb8
ove2xrate <133969780+ove2xrate@users.noreply.github.com>
2023-08-14 17:58:50 +0400

    Внесены изменения: - создан ProfileViewController; - создан UIViewController в Main.storyobard; - в новый UIViewController добавлены UI-элементы - установлены констрейнты - созданы @IBOutlet'ы и @IBAction. - ассеты добавлена в файл проекта

---
+++ image_feed/Profile/ProfileViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+// MARK: - IBOutlet
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    
+// MARK: - IBAction
+    @IBAction func didTapLogoutButton(_ sender: Any) {
+    }
+}
