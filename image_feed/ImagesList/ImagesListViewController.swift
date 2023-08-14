7e7f0a9d6ec131eed71dbfbd6a4f78e7a8ebb20c
ove2xrate <133969780+ove2xrate@users.noreply.github.com>
2023-08-14 13:23:07 +0400

    Внесены правки после ревью: — перенесён configCell — status bar style с default изменен на light content

--- image_feed/ImagesList/ImagesListViewController.swift
+++ image_feed/ImagesList/ImagesListViewController.swift
@@ -19,20 +19,6 @@ class ImagesListViewController: UIViewController {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
-    
-// MARK: - Public Methods
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        guard let image = UIImage(named: photosName[indexPath.row]) else {
-            return
-        }
-        
-        cell.cellImage.image = image
-        cell.dateLabel.text = dateFormatter.string(from: Date())
-        
-        let isLiked = indexPath.row % 2 == 0
-        let likeImage = isLiked ? UIImage(named: "Like") : UIImage(named: "Unlike")
-        cell.likeButton.setImage(likeImage, for: .normal)
-    }
 }
 
 // MARK: - Extensions
@@ -50,12 +36,25 @@ extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) // 1
         
-        guard let imageListCell = cell as? ImagesListCell else { // 2
+        guard let imageListCell = cell as? ImagesListCell else {
             return UITableViewCell()
         }
         
-        configCell(for: imageListCell, with: indexPath) // 3
-        return imageListCell // 4
+        configCell(for: imageListCell, with: indexPath)
+        return imageListCell
+        
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+            }
+            
+            cell.cellImage.image = image
+            cell.dateLabel.text = dateFormatter.string(from: Date())
+            
+            let isLiked = indexPath.row % 2 == 0
+            let likeImage = isLiked ? UIImage(named: "Like") : UIImage(named: "Unlike")
+            cell.likeButton.setImage(likeImage, for: .normal)
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
